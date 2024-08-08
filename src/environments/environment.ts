fa12fca970d54bc3265fe126ded67a4f3118640a
Atul Chhotu Patil <atulp12@kpit.com>
2024-08-08 14:18:25 +0530

    code added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
 //production: false,
 //apiUrl : 'http://localhost:8080'
-//apiUrl  : 'http://10.52.220.5:31163'
-  apiUrl : 'http://10.52.97.23:31156'
+  apiUrl : 'http://10.52.97.29:31389'
 
 };
